2da59d632f517cd987e237b89ee6415af244ac1b
Ivan Dimitrov <idimitrov07@gmail.com>
2015-10-10 22:46:02 +0300

    Move logic to the controller, add helper method.

--- app/controllers/greeter_controller.rb
+++ app/controllers/greeter_controller.rb
@@ -1,5 +1,10 @@
 class GreeterController < ApplicationController
   def hello
+    randon_names = ["Alex", "Joe", "Diana"]
+    @name = randon_names.sample
+    @time = Time.now
+    @times_displayed ||= 0
+    @times_displayed += 1
   end
 
   def goodbye

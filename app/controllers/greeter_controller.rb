590d35b2538b168fb5ee3e0a4b4a176bec3d18d8
Ivan Dimitrov <idimitrov07@gmail.com>
2015-10-11 00:16:31 +0300

    Rename entries.

--- app/controllers/greeter_controller.rb
+++ app/controllers/greeter_controller.rb
@@ -1,6 +1,6 @@
 class GreeterController < ApplicationController
   def hello
-    randon_names = ["Alex", "Joe", "Diana"]
+    randon_names = ["Alex", "Joe", "Sam"]
     @name = randon_names.sample
     @time = Time.now
     @times_displayed ||= 0
